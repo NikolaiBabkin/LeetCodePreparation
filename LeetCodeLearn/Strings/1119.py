1fae0f3c99f1dee58bcba614f73c6f984027f3a1
a18249761 <nikolababkin@gmail.com>
2020-11-30 17:39:35 +0000

    Commit problem 1119

---
+++ LeetCodeLearn/Strings/1119.py
@@ -0,0 +1,31 @@
+'''
+Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+Example 1:
+Input: "leetcodeisacommunityforcoders"
+Output: "ltcdscmmntyfrcdrs"
+
+Example 2:
+Input: "aeiou"
+Output: ""
+
+
+Note:
+S consists of lowercase English letters only.
+1 <= S.length <= 1000
+'''
+
+class Solution:
+    def removeVowels(self, S: str) -> str:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        res = ''
+        for s in S:
+            if s not in vowels:
+                res += s
+
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print("" == s.removeVowels("aeiou"))
