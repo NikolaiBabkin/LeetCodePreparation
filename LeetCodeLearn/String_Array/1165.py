197bb5c018eb12640f2eee76d724133ba3a66581
a18249761 <nikolababkin@gmail.com>
2020-12-03 19:26:45 +0000

    Add problem 1165 solution

---
+++ LeetCodeLearn/String_Array/1165.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        k_dict = {letter: i for i, letter in enumerate(keyboard)}
+        res, current_pos = 0, 0
+        for letter in word:
+            res += abs(current_pos - k_dict[letter])
+            current_pos = k_dict[letter]
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(f'Test 1: {s.calculateTime("abcdefghijklmnopqrstuvwxyz", "cba") == 4}')
+    print(f'Test 2: {s.calculateTime("pqrstuvwxyzabcdefghijklmno", "leetcode") == 73}')
