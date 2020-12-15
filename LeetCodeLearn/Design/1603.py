eab8aaec60179a1990e92670c9a5cee6113f14e2
a18249761 <nikolababkin@gmail.com>
2020-12-15 19:10:34 +0000

    Add problem 1603 solution

---
+++ LeetCodeLearn/Design/1603.py
@@ -0,0 +1,30 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.cnt_big = big
+        self.cnt_medium = medium
+        self.cnt_small = small
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.cnt_big > 0:
+                self.cnt_big -= 1
+                return True
+            else:
+                return False
+        if carType == 2:
+            if self.cnt_medium > 0:
+                self.cnt_medium -= 1
+                return True
+            else:
+                return False
+        if carType == 3:
+            if self.cnt_small > 0:
+                self.cnt_small -= 1
+                return True
+            else:
+                return False
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
