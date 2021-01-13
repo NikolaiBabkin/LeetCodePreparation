6c12287f5a0cc667d66bffcdb2de5e868442082c
a18249761 <nikolababkin@gmail.com>
2021-01-13 11:58:28 +0000

    Add problem 1351 solution

---
+++ LeetCodeLearn/String_Array/1351.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        """
+        Time Complexity: O(m + n)
+        Space Complexity: O(1)
+        """
+        res = 0
+        n = len(grid)
+        m = len(grid[0])
+        i = 0
+        j = m-1
+        while i < n:
+            while j >= 0 and grid[i][j] < 0:
+                res += n - i
+                j -= 1
+            i += 1
+        return res
