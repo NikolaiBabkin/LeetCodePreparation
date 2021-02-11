c683884975ff19cef501b24aed62b8a43f92a9ab
nikolai_babkin <nikolababkin@gmail.com>
2021-02-11 18:58:23 +0000

    Add problem 203 solution

---
+++ LeetCodeLearn/linked_list/203.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head is None:
+            return head
+        sentinel = ListNode(-1, head)
+        node = sentinel
+        while node.next:
+            if node.next.val == val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        return sentinel.next
+
