4fd95b5f3e41753a6bd2ecc0dc6d0c308fb2b309
nikolai_babkin <nikolababkin@gmail.com>
2021-02-11 18:14:32 +0000

    Add problem 16 solution

---
+++ LeetCodeLearn/linked_list/19.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        sentinel = ListNode(-1, head)
+        fast = sentinel
+        slow = sentinel
+        for _ in range(n):
+            fast = fast.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return sentinel.next
