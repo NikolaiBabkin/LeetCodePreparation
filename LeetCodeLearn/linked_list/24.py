0ddfcbee48b4232178d23a5b3284e7d0ff5ea2e9
nikolai_babkin <nikolababkin@gmail.com>
2021-02-15 11:35:46 +0000

    Add problem 24 solution

---
+++ LeetCodeLearn/linked_list/24.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs_recursion(self, head: ListNode) -> ListNode:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        sentinel = ListNode(-1, head)
+        if head and head.next:
+            sentinel.next = head.next
+            head.next = head.next.next
+            sentinel.next.next = head
+            head = sentinel.next
+            head.next.next = self.swapPairs(head.next.next)
+        return sentinel.next
+
+    def swapPairs_iterative(self, head: ListNode) -> ListNode:
+        sentinel = ListNode(-1, head)
+        prev = sentinel
+        curr = head
+        while curr and curr.next:
+            prev.next = curr.next
+            curr.next = curr.next.next
+            prev.next.next = curr
+            prev = prev.next.next
+            curr = prev.next
+
+        return sentinel.next
