8b0c7fd2b20c6b1fe156c76e25d880c60b99088d
nikolai_babkin <nikolababkin@gmail.com>
2021-02-19 16:07:32 +0000

    Fix median fo medians implementation to handle a lot of same numbers in array in O(n) time

--- LeetCodeLearn/DS_Algorithms/median_of_median.py
+++ LeetCodeLearn/DS_Algorithms/median_of_median.py
@@ -5,7 +5,7 @@ import random
 def median_of_median(arr, k):
     def kth_statistic(lo, hi, k):
         while lo < hi:
-            p_idx = partition(lo, hi)
+            p_idx = partition(lo, hi, k)
             if k < p_idx + 1:
                 hi = p_idx - 1
             elif k == p_idx + 1:
@@ -29,7 +29,7 @@ def median_of_median(arr, k):
         p_idx = kth_statistic(lo, lo + sub_len - 1, lo + sub_len // 2 + 1)
         return p_idx
 
-    def partition(lo, hi):
+    def partition(lo, hi, k):
         p_idx = find_pivot(lo, hi)
         arr[p_idx], arr[hi] = arr[hi], arr[p_idx]
         i = lo
@@ -37,15 +37,28 @@ def median_of_median(arr, k):
             if arr[j] < arr[hi]:
                 arr[j], arr[i] = arr[i], arr[j]
                 i += 1
-        arr[i], arr[hi] = arr[hi], arr[i]
-        return i
+
+        i_eq = i
+        for j in range(i, hi):
+            if arr[j] == arr[hi]:
+                arr[i_eq], arr[j] = arr[j], arr[i_eq]
+                i_eq += 1
+
+        arr[i_eq], arr[hi] = arr[hi], arr[i_eq]
+
+        if k < i + 1:
+            return i
+        if k <= i_eq + 1:
+            return k - 1
+        return i_eq
 
     k_idx = kth_statistic(0, len(arr) - 1, k)
     return arr[k_idx]
 
 
 if __name__ == '__main__':
-    arr = [random.randint(1, 1000) for _ in range(5000)]
+    # arr = [random.randint(1, 1000) for _ in range(5000)]
+    arr = [10] * 10000
     k = random.randint(1, 1000)
     print(median_of_median(arr, k))
     print(heapq.nsmallest(k, arr)[-1])
