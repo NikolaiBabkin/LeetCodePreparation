dce5f87189e68cda91a546d1c50515386e7a3bb7
nikolai_babkin <nikolababkin@gmail.com>
2021-02-22 17:15:12 +0000

    Add problem 94 solution

---
+++ LeetCodeLearn/Tree/94.py
@@ -0,0 +1,52 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal_iterative(self, root: TreeNode) -> List[int]:
+        res = []
+        node = root
+        stack = []
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            res.append(node.val)
+            node = node.right
+
+        return res
+
+    def inorderTraversal_morris(self, root: TreeNode) -> List[int]:
+        res = []
+        node = root
+        while node:
+            if node.left:
+                curr = node.left
+                while curr.right and curr.right != node:
+                    curr = curr.right
+                if curr.right is None:
+                    curr.right = node
+                    node = node.left
+                else:
+                    curr.right = None
+                    res.append(node.val)
+                    node = node.right
+            else:
+                res.append(node.val)
+                node = node.right
+
+        return res
+
+    def inorderTraversal_recursion(self, root: TreeNode) -> List[int]:
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                res.append(node.val)
+                inorder(node.right)
+
+        res = []
+        inorder(root)
+        return res
