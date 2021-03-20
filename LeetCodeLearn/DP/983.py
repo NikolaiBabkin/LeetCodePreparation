785a6bdceea58d4f82b74654f5e4debcf5966368
nikolai_babkin <nikolababkin@gmail.com>
2021-03-20 12:21:12 +0000

    Add problem 983 solution

---
+++ LeetCodeLearn/DP/983.py
@@ -0,0 +1,30 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        @lru_cache(None)
+        def cost_calc(k):
+            if k >= len(days):
+                return 0
+
+            p = cost_calc(k + 1) + costs[0]
+
+            l = 0
+            while k + l < len(days) and days[k + l] - days[k] < 7:
+                l += 1
+
+            p = min(p, cost_calc(k + l) + costs[1])
+
+            l = 0
+            while k + l < len(days) and days[k + l] - days[k] < 30:
+                l += 1
+
+            p = min(p, cost_calc(k + l) + costs[2])
+
+            return p
+
+        return cost_calc(0)
+
+
