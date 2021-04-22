1a7f61d2bc494b7d6521dcd95aee6f49a1c48a78
nikolai_babkin <nikolababkin@gmail.com>
2021-04-22 17:28:52 +0100

    Add problem 25 solution, update

---
+++ LeetCodeLearn/linked_list/25.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        def reverse(pre_head):
+            tail = pre_head.next
+            for _ in range(k - 1):
+                new_head = tail.next
+                tail.next = tail.next.next
+                new_head.next = pre_head.next
+                pre_head.next = new_head
+
+            return tail
+
+        if head is None or k == 1:
+            return head
+
+        santinel = ListNode(-1, head)
+        size = 0
+        node = head
+        while node:
+            size += 1
+            node = node.next
+
+        cnt_to_reverse = size // k
+        pre_head = santinel
+        for _ in range(cnt_to_reverse):
+            pre_head = reverse(pre_head)
+
+        return santinel.next
+
